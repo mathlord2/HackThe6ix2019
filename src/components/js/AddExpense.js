678274e52ce54f1dd211d42ab09a526d3416782c
Eric Xiao <erixrekt69@gmail.com>
2019-08-25 06:14:35 -0400

    Small styling changes

--- src/components/js/AddExpense.js
+++ src/components/js/AddExpense.js
@@ -14,7 +14,7 @@ class AddExpense extends React.Component {
                 <form>
                     <input type="text" placeholder="Expense Name" name="Expense"></input>
                     <input type="text" placeholder={"Price " + '(' + this.props.currency + ')'} name={"Price " + '(' + this.props.currency + ')'}></input>
-                    <button className="add-expense"><div>Add</div></button>
+                    <button className="add-expense">Add</button>
                 </form>
             </center>
         );
