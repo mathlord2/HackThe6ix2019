3dcad81a80d766a17b5fb085f88254c570e44cbc
Eric Xiao <erixrekt69@gmail.com>
2019-08-24 20:12:00 -0400

    Added in sign-up page

--- frontend/js/firebase.js
+++ frontend/js/firebase.js
@@ -46,7 +46,71 @@ function logout() {
     }); 
 }
 
+function switchToSignUp(none, block) {
+    none.style.display = "none";
+    block.style.display = "block";
+}
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Submit form
+function submitForm(e) {
+    var name = getInputVal('name');
+    var email = getInputVal('email');
+    var password = getInputVal('password');
+    var homeLocation = getInputVal('homeLocation');
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        if (errorCode == 'auth/email-already-in-use') {
+            alert('This username is already in use.');
+        } else {
+            alert(errorMessage);
+        }
+        console.log(error);
+    });
+
+    e.preventDefault();
+
+    // Save message
+    saveMessage(name, password, email, homeLocation);
+
+    // Show alert
+    window.alert('Sign up successful!');
+
+    document.getElementById('newUserForm').style.display = 'none';
+    document.getElementById('main').style.display = 'block';
+
+    // Clear form
+    document.getElementById('newUserForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id) {
+    return document.getElementById(id).value;
+}
+
+// Save data to firebase
+function saveMessage(name, email, password, homeLocation) {
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        name, name,
+        email: email,
+        password: password,
+        homeLocation: homeLocation
+    });
+}
+
 window.onload = function() {
     $('#loginBtn').click(login);
     $('#signOut').click(logout);
+    $('#switch1').click(function() {
+        switchToSignUp(document.getElementById('login_div'), document.getElementById('newUserForm'));
+    });
+    $('#switch2').click(function() {
+        switchToSignUp(document.getElementById('newUserForm'), document.getElementById('login_div'));
+    });
+    $('#newUserForm').submit(submitForm);
 }
